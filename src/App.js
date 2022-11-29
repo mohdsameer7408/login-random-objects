ac4390a9cd5c09b8138057052607806e1b0010e1
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2022-11-29 13:36:58 +0530

    added login header which has images oriented diagonally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login__header-container">
+        {Array(10)
+          .fill()
+          .map(() => (
+            <div className="image__diagonal">
+              <img
+                src="https://picsum.photos/200/300"
+                alt=""
+                className="picture"
+              />
+              <img
+                src="https://picsum.photos/200/300"
+                alt=""
+                className="picture"
+              />
+              <img
+                src="https://picsum.photos/200/300"
+                alt=""
+                className="picture"
+              />
+              <img
+                src="https://picsum.photos/200/300"
+                alt=""
+                className="picture"
+              />
+            </div>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
